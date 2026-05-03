73508dfb5de05758a071df5212cda68da1e2d405
aldifirstanto <aldifirstanto@gmail.com>
2026-05-03 22:12:50 +0700

    Add barcode scanner component using html5-qrcode

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import productRoutes from './routes/productRoutes.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', productRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({
+    message: err.message || 'Internal server error'
+  });
+});
+
+export default app;
